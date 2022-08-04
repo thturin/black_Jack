0bb77ae40ae13b5b2e66fbcf4fab232f159a3323
tatiana <tatiana.turin@gmail.com>
2022-08-04 09:31:50 -0400

    c

--- blackjack.py
+++ blackjack.py
@@ -1,19 +1,20 @@
+import random
+
 class Card:
     def __init__(self,suit,val):
         self.suit=suit
         self.value =val
 
     def show(self):
-        print('{} of {}'.format(self.value,self.suit))
+        print('{} of {}'.format(self.value,self.suit),end='')
+
 
     def get_value(self):
         dict = {
-            'A':[1,11], '2':2, '3':3, '4':4, '5':5, '6':6, '7':7,
+            'A':11, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7,
             '8':8, '9':9, '10':10, 'J':10, 'Q':10, 'K':10
         }
-
-        print(dict)
-
+        return dict[self.value]
 
 
 
@@ -29,3 +30,76 @@ class Deck:
     def show(self):
         for card in self.cards:
             card.show()
+
+    def shuffle(self): #shuffle the cards
+        #Fishe-Yates shuffle algorithm
+        for i in range(len(self.cards)-1,0,-1): # for int i=len(card)-1 i>0 i--
+            #pick a random index from 0 to i
+            rand_int = random.randint(0,i+1)
+            #swap with current i
+            self.cards[i],self.cards[rand_int]=self.cards[rand_int],self.cards[i]
+
+    def draw(self): #draw the cards
+        return self.cards.pop() #return the top card of the deck
+
+    #the maximum number of cards you can hold in a hand without busting is 11: 4 aces, 4 2's and 3 3's
+
+
+
+
+
+
+class Player:
+    def __init__(self, isDealer, name, deck):
+        self.isDealer = isDealer
+        self.name = name
+        self.deck = deck
+        self.total = 0
+        self.hand = []
+
+
+    def show_Hand(self):
+        if self.isDealer:
+            print('Dealer is dealt a ', end='')
+            self.hand[0].show()
+            print(' and a ', end='')
+            self.hand[1].show()
+        else:
+            print('Player is dealt a ', end='')
+            self.hand[0].show()
+            print(' and a ', end='')
+            self.hand[1].show()
+        print()
+
+    def handle_ace(self):
+        if self.total + 11 < 21:
+            return 11
+        else:
+            return 1
+
+    def calculate_total(self):
+        for card in self.hand:
+            if card.value == 'A':
+                self.total+=self.handle_ace()
+            else:
+                self.total+=card.get_value()
+        print('{}s total is {}'.format(self.name, self.total))
+        return self.total
+
+
+    def dealer_check_total(self):
+        pass
+
+
+    def hit_or_stand(self):
+        print('{}, would you like to hit or stand? Enter h or s'.format(self.name))
+        response = input()
+        if response == 'h':
+            self.hand.append(self.deck.draw())
+            return 'h'
+        else:
+            return 's'
+
+
+
+
--- main.py
+++ main.py
@@ -1,27 +1,103 @@
 import random
-from blackjack import Card, Deck
+from blackjack import Card, Deck, Player
 """
-Project No. BlackJack (non oop)
+Project No. BlackJack ( oop)
 
 """
 
 
 #global vars
-dealer_cards = []
-player_cards = []
-deck_dict = {'A': [1,11], '2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':10,"Q":10,'K':10}
-deck = []
-
 
 #flags
 game_over = False
-
-card = Card('hearts','A')
+draw_two = True
 
 deck = Deck()
 deck.build()
-#deck.show()
+deck.shuffle()
 
-deck.cards[0].get_value
-print()
+
+dealer = Player(True,'Dealer',deck)
+player = Player(False, 'Player', deck)
+
+flag = True
+dealer_stay = False
+player_stay = False
+
+#deal first two cards for player and dealer
+# dealer.hand.append(deck.draw())
+# dealer.hand.append(deck.draw())
+# player.hand.append(deck.draw())
+# player.hand.append(deck.draw())
+
+# dealer.show_Hand()
+# player.show_Hand()
+
+counter = 0
+while not game_over:
+    while draw_two:
+        for i in range(2):
+            dealer.hand.append(deck.draw())
+            player.hand.append(deck.draw())
+        dealer.show_Hand()
+        player.show_Hand()
+        draw_two = False
+
+    while flag:
+        dealer_turn = dealer.hit_or_stand()
+        player_turn = player.hit_or_stand()
+
+        if dealer_stay == False:
+            if dealer_turn == 'h':
+                dealer.hand.append(deck.draw())
+                dealer.calculate_total()
+            elif dealer_turn == 's':
+                counter+=1
+                dealer_stay=True
+        if player_turn == False:
+            if player_turn == 'h':
+                player.hand.append(deck.draw())
+                player.calculate_total()
+            elif player_turn == 's':
+                counter+=1
+                dealer_stay = True
+
+        if counter == 2:
+            game_over = True
+            flag = False
+
+
+
+
+
+
+
+
+
+
+
+    # dealer_total = dealer.calculate_total()
+    # player_total = player.calculate_total()
+
+
+    game_over = True
+
+
+
+
+
+
+#player_next_card = Input('Player hit or stay? Enter h or t: ')
+
+
+
+    #dealer draws
+
+    #player draws
+
+
+#create a draw method
+
+
+
 #print(deck.cards[10].value)
